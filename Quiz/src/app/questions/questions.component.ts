904c0b6594ce8ed68be05d8d3983e7ecb33a967d
Niraj Kamal <nirajkkamal@gmail.com>
2022-02-21 00:54:08 +0530

    dynamic questions

--- Quiz/src/app/questions/questions.component.ts
+++ Quiz/src/app/questions/questions.component.ts
@@ -8,17 +8,47 @@ import { QuestionService } from '../service/question.service'
 })
 export class QuestionsComponent implements OnInit {
 
+  public questionList : any = [];
+  public currentQuestion : number =0;
+  public points : number = 0;
+  counter = 60;
+  public answers : object = {};
+  public progress : string = '0';
+
+
   constructor(private questionService: QuestionService) { }
 
   ngOnInit(): void {
     this.getAllQuestions();
   }
 
+  getProgressPercent(){
+    this.progress = (((this.currentQuestion+1)/this.questionList.length)*100).toString();
+    return this.progress;
+  }
+
   getAllQuestions(){
     this.questionService.getQuestionJson()
     .subscribe(response=>{
-      console.log(response);
+      this.questionList = response;
+      console.log(response[0])
     })
+    this.getProgressPercent();
   }
+  nextQuestion(){
+    this.currentQuestion++;
+    this.getProgressPercent();
+  }
+  previousQuestion(){
+    this.currentQuestion--;
+    this.getProgressPercent();
+  }
+
+  
+
+  // answerMCQ(currentQno:number,option:any){
+  //   if (option)
+
+  // }
 
 }
